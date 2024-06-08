d2410c378bb26a90f9c3f1068d9824900116fb17
holyseo <128103221+holyseo@users.noreply.github.com>
2024-06-08 09:12:57 +0100

    fix another cors

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,7 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-// app.use(cors());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://foodie-fe.netlify.app");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors());
 
 connectDB();
 
