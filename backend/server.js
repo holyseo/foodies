6471280a55558b48b7e149cadc56d16a2104fc02
holyseo <128103221+holyseo@users.noreply.github.com>
2024-06-08 09:08:57 +0100

    upate cors function

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,18 @@ const port = 4000;
 app.use(express.json());
 // app.use(cors());
 
-const corsOptions = {
-  origin: "https://foodie-fe.netlify.app",
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://foodie-fe.netlify.app");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 connectDB();
 
