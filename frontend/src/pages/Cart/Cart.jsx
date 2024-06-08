2ddffe858c390446590f7219c09cd4a142e84253
holyseo <128103221+holyseo@users.noreply.github.com>
2024-06-08 14:31:06 +0100

    fix conflict with  pre-existing data

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "../../context/StoreContext";
 import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart, getTotalCartAmount, url } =
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
   const navigate = useNavigate();
   return (
@@ -25,7 +25,7 @@ const Cart = () => {
             return (
               <>
                 <div key={index} className="cart-items-title cart-items-item">
-                  <img src={`${url}/images/${item.image}`} alt="" />
+                  <img src={item.image} alt="" />
                   <p>{item.name}</p>
                   <p>£{item.price}</p>
                   <p>{cartItems[item._id]}</p>
